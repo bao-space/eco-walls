0c4cc53a7be3b1d4b88777211978480731f663a6
João Pedro Alves <jotalmeida007@hotmail.com>
2023-11-09 09:34:27 -0300

    fix: try to fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 function App() {
   return (
     <>
-      <BrowserRouter basename="/eco-walls/">
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/eco-walls/"}>
         <ShoppingCartProvider>
           <Router />
         </ShoppingCartProvider>
